ebd92f0153ffa4dbabea25f9b6343da7f02557e5
Zeustakeshi <gacon2312@gmail.com>
2022-09-22 08:56:07 +0700

    prev code

--- script2.js
+++
@@ -1,9 +0,0 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-const canvasWidth = (canvas.width = 1000);
-const canvasHeight = (canvas.height = 600);
-
-ctx.fillStyle = "red";
-
-ctx.fillRect(canvasWidth / 2, canvasHeight / 2, 100, 100);
